7a7f26db2302e8ffcf6fe9274a872d883853db6f
ekoops <dgleo.96@gmail.com>
2021-03-25 22:03:09 +0100

    AdaIN style transferring support added #2

---
+++ ssd/data/loaders/__init__.py
@@ -0,0 +1,1 @@
+from .adain_loader import AdainLoader
---
+++ ssd/data/loaders/adain_loader.py
@@ -0,0 +1,64 @@
+from torch.utils.data import DataLoader
+import numpy as np
+import torch
+import os
+
+
+class AdainLoader(DataLoader):
+    def __init__(self, cfg, content_loader, style_loader):
+        super().__init__()
+
+        self.content_loader = content_loader
+        self.style_loader = style_loader
+        self.alpha = cfg.ADAIN.MODEL.ALPHA
+        self.transfer_ratio = cfg.ADAIN.LOADER.TRANSFER_RATIO
+        self.device = torch.device(cfg.MODEL.DEVICE)
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        os.chdir(cfg.ADAIN.IMPL_FOLDER)
+
+        from test import get_net, style_transfer, coral
+        vgg_path = cfg.ADAIN.MODEL.BACKBONE.VGG
+        decoder_path = cfg.ADAIN.MODEL.BACKBONE.DECODER
+        # 1) Import get_net function in order to
+        # prepare the AdaINNet.
+        self.adain_net = get_net(decoder_path=decoder_path, vgg_path=vgg_path)
+        # 2) Import style_transfer function in order to
+        # apply the style transferring
+        self.style_transfer = style_transfer
+        # 3) Import coral function in order to
+        # preserve color
+        self.preserve_color = cfg.ADAIN.INPUT.STYLE.PRESERVE_COLOR
+        self.coral = coral if self.preserve_color else None
+
+        os.chdir(dir_path)
+
+    def __iter__(self):
+        for (train_batch, train_targets, train_indexes), \
+                (style_batch, _, _) in zip(self.content_loader, self.style_loader):
+            train_batch = self.style_batch(
+                content_batch=train_batch,
+                style_batch=style_batch,
+            )
+            yield train_batch, train_targets, train_indexes
+
+    def __len__(self):
+        return self.content_loader.__len__
+
+    def style_batch(self, content_batch, style_batch):
+        for idx, (content_image, style_image) in enumerate(zip(content_batch, style_batch)):
+            if np.random.rand() <= self.transfer_ratio:
+                if self.preserve_color:
+                    style_image = self.coral(style_image, content_image)
+                style_image = style_image.to(self.device).unsqueeze(0)
+                content_image = content_image.to(self.device).unsqueeze(0)
+                with torch.no_grad():
+                    content_batch[idx] = self.style_transfer(
+                        vgg=self.adain_net.vgg,
+                        decoder=self.adain_net.decoder,
+                        content=content_image,
+                        style=style_image,
+                        alpha=self.alpha
+                    ).cpu()
+                # qui posso fare in caso interpolation(train_batch)
+        return content_batch
